ba45fd73c18dc291f7ae8472f572816153fa73d5
Olivier Hoareau <oha@greenberets.io>
2021-09-22 17:36:39 +0200

    feat: exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
 export * from './utils';
 export * from './hooks';
 export * from './components';
+export * from './AbstractBundle';
+export * from './cli';
+export * from './Context';
+export * from './IBundle';
+export * from './IContext';
+export * from './types';
 export {default as default} from './utils/generate';
--- src/utils/cli.ts
+++ src/utils/cli.ts
@@ -1,12 +1,13 @@
 import generate from "./generate";
+import {options} from '../types';
 
-export async function cli() {
+export async function cli(options: options = {}) {
     if (4 > process.argv.length) {
         // noinspection ExceptionCaughtLocallyJS
         throw new Error(`Syntax: node cli.js <data> <config> <output>`);
     }
 
-    return generate(process.argv[2], process.argv[3], process.argv[4])
+    return generate(process.argv[2], process.argv[3], process.argv[4], options)
 }
 
 export default cli;
