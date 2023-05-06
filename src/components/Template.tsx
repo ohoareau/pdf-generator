5467234fe5447474af07db5692573627229eadee
Olivier Hoareau <oha@greenberets.io>
2023-05-06 09:19:22 +0200

    fix: fix

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,13 +1,14 @@
 import {usePdfComponent} from "../hooks/usePdfComponent";
 
-export function Template({type, config}: TemplateProps) {
-    const Comp = usePdfComponent('template', type, 'default', false);
+export function Template({type, config, template}: TemplateProps) {
+    const Comp = usePdfComponent('template', type, 'default', false, template);
 
     return Comp ? <Comp {...config} /> : null;
 }
 
 export interface TemplateProps {
     type?: string;
+    template?: any;
     [key: string]: any;
 }
 
--- src/hooks/usePdfComponent.ts
+++ src/hooks/usePdfComponent.ts
@@ -1,10 +1,10 @@
 import usePdfContext from "./usePdfContext";
 
-export function usePdfComponent(type: string, name: string|undefined, defaultName: string = 'default', fail = true) {
+export function usePdfComponent(type: string, name: string|undefined, defaultName: string = 'default', fail = true, forcedTemplate?: any) {
     const c = usePdfContext();
 
     try {
-        return c.getComponent(type, name || defaultName, defaultName);
+        return forcedTemplate || c.getComponent(type, name || defaultName, defaultName);
     } catch (e) {
         if (fail) throw e;
         return null;
